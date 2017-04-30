53d55d497c84b2a57c0a37f3dc6f319b2d2de72a
HenryVegaAyala <hp.vega@hotmail.com>
2017-04-30 17:12:46 -0500

    New changelist

--- config/db.php
+++ config/db.php
@@ -2,8 +2,11 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=blog',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
+    'enableSchemaCache' => true,
+    'schemaCacheDuration' => 3600,
+    'schemaCache' => 'cache',
 ];
